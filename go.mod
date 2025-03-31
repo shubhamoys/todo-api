f36425bc86607e6fb985084e95c63c6ff768e562
Shubhamoy <shubhamoys@gmail.com>
2025-03-31 22:10:15 +0530

    ~ Enhancement: Log formatter modified

--- go.mod
+++ go.mod
@@ -4,9 +4,12 @@ go 1.23.1
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-playground/validator/v10 v10.20.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.3
 	go.mongodb.org/mongo-driver v1.17.3
+	golang.org/x/crypto v0.36.0
 )
 
 require (
@@ -18,7 +21,6 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -30,7 +32,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -38,7 +39,6 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
--- utils/logger.go
+++ utils/logger.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -31,9 +32,9 @@ func InitLogger() {
 	}
 
 	// Set the output for different log levels
-	Logger.SetOutput(infoLogFile) // Default output is info.log
-	Logger.SetFormatter(&logrus.JSONFormatter{})
-	Logger.SetLevel(logrus.DebugLevel) // Set the default log level
+	Logger.SetOutput(infoLogFile)          // Default output is info.log
+	Logger.SetFormatter(&EmojiFormatter{}) // Use the custom emoji formatter
+	Logger.SetLevel(logrus.DebugLevel)     // Set the default log level
 
 	// Add hooks to write error logs to error.log
 	Logger.AddHook(&LogFileHook{
@@ -59,4 +60,31 @@ func (hook *LogFileHook) Fire(entry *logrus.Entry) error {
 
 func (hook *LogFileHook) Levels() []logrus.Level {
 	return hook.LogLevels
+}
+
+// EmojiFormatter is a custom formatter that adds emojis based on log level
+type EmojiFormatter struct{}
+
+func (f *EmojiFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	var emoji string
+	switch entry.Level {
+	case logrus.InfoLevel:
+		emoji = "✅" // Emoji for info logs
+	case logrus.WarnLevel:
+		emoji = "⚠️" // Emoji for warning logs
+	case logrus.ErrorLevel:
+		emoji = "❌" // Emoji for error logs
+	case logrus.DebugLevel:
+		emoji = "🐛" // Emoji for debug logs
+	case logrus.FatalLevel:
+		emoji = "💀" // Emoji for fatal logs
+	case logrus.PanicLevel:
+		emoji = "🔥" // Emoji for panic logs
+	default:
+		emoji = "ℹ️" // Default emoji
+	}
+
+	// Format the log message with the emoji
+	logMessage := fmt.Sprintf("%s [%s] %s\n", emoji, entry.Level.String(), entry.Message)
+	return []byte(logMessage), nil
 }
